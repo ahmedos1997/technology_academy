2029d42d9b11cf4bb776fb78c80d17bf6a7b338c
ahmed osman <ahmed-elmadani@outlook.com>
2023-11-15 23:29:37 +0200

    new git

--- courses/views.py
+++ courses/views.py
@@ -141,19 +141,17 @@ def success(request):
 def cancel(request):
     return render(request, 'page/cancel.html')
 
-
 def change_language(request):
     if request.method == 'POST':
         language = request.POST.get('language')
         if language:
             request.session['django_language'] = language
+            activate(language)
     else:
         language = request.session.get('django_language')
-
-    if 'next' in request.GET:
-        return redirect(request.GET['next'])
-
-    return redirect('main')
+        if language:
+            activate(language)
+    return HttpResponseRedirect(reverse('main'))
 
 
 def send_order_mail(request, course):
