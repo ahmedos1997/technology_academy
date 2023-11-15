1e3fd4410e80699bc2a953021ea2d2c9c0f5d01a
ahmed osman <ahmed-elmadani@outlook.com>
2023-11-15 21:06:42 +0200

    new

--- technology_academy/urls.py
+++ technology_academy/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('courses.urls')),
-    path('blog', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('users/', include('users.urls'))
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
